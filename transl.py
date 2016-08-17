38ce4ae6a44c7b536c430af44aa55dcc1c8af60a
lkk <kluneev@gmail.com>
2016-08-17 16:10:13 +0300

    new_word

--- transl.py
+++ transl.py
@@ -4,6 +4,7 @@ original = input("Enter a word:")
 word = original.lower()
 first = word[0]
 new_word = word+first+py
+new_word = new_word[1:len(new_word)]
 if len(original) > 0 and original.isalpha():
     print (original)
 else:
