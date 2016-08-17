0d7ff8feb0d0b8231c9b3bc88a4360b9f3d93b40
lkk <kluneev@gmail.com>
2016-08-17 16:03:57 +0300

    test2_add

--- transl.py
+++ transl.py
@@ -1,9 +1,9 @@
 print ('Welcome to the Pig Latin Translator!')
-
+py = 'ay'
 original = input("Enter a word:")
 word = original.lower()
 first = word[0]
-
+new_word = word+first+py
 if len(original) > 0 and original.isalpha():
     print (original)
 else:
