40dcde8a734e0ad8e166eee8648bac91681bd4aa
lkk <kluneev@gmail.com>
2016-08-17 15:42:47 +0300

    word adn first

--- transl.py
+++ transl.py
@@ -1,6 +1,9 @@
 print ('Welcome to the Pig Latin Translator!')
 
 original = input("Enter a word:")
+word = original.lower()
+first = word[0]
+
 if len(original) > 0 and original.isalpha():
     print (original)
 else:
